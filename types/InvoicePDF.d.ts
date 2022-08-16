dff89114528bb91a0be618594cce7cafea2ad577
Maël Gangloff <contact@maelgangloff.fr>
2022-08-16 17:23:24 +0200

    fix: remove types folder

--- types/InvoicePDF.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="pdfkit" />
-import Invoice from './index';
-export declare function invoicePDF(invoice: Invoice): Promise<PDFKit.PDFDocument>;
--- types/Line.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Line {
-    item: string;
-    description?: string;
-    quantity: number;
-    tax: number;
-    unitPrice: number;
-}
